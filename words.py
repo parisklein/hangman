bc1b2bf2315653feec871a84578972e7d79df860
parisklein <92563216+parisklein@users.noreply.github.com>
2021-10-14 21:05:11 -0700

    Create words.py

---
+++ words.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Paris Klein
+"""
+
+words = [
+    'dog',
+    'cat',
+    'bat',
+    'run',
+    'bag',
+    'bull',
+    'tree',
+    'see',
+    'good',
+    'and',
+    'key',
+    'mouse',
+    'moon']
