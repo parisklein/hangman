091feb0104880e00ad122737ac8f6fd99df46283
parisklein <92563216+parisklein@users.noreply.github.com>
2021-10-14 21:05:46 -0700

    Create words2.py

---
+++ words2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Paris Klein
+"""
+words2 = [   
+    'hello',
+    'world',
+    'computer',
+    'program',
+    'science',
+    'computing',
+    'intelligence',
+    'algorithm',
+    'process',
+    'data',
+    'analysis',
+    'architecture',
+    'model']
